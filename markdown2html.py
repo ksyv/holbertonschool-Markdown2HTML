6bf7a0eb39e492dd701b6181442d775f09ff6c50
ksyv <viais.kevin@hotmail.fr>
2024-08-07 12:49:00 +0200

    add file for task 0, check if the command content 2 arg and first is a existing Mardown file. GIve the second arg by name at the output file

---
+++ markdown2html.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Write a script markdown2html.py that takes an argument 2 strings:
+
+First argument is the name of the Markdown file
+Second argument is the output file name
+Requirements:
+
+If the number of arguments is less than 2: print in STDERR Usage: ./markdown2html.py README.md README.html and exit 1
+If the Markdown file doesn’t exist: print in STDER Missing <filename> and exit 1
+Otherwise, print nothing and exit 0"""
+import sys
+import os
+import markdown
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
+        sys.exit(1)
+
+    markdown_file = sys.argv[1]
+    if not os.path.exists(markdown_file):
+        print("Missing {}".format(markdown_file), file=sys.stderr)
+        sys.exit(1)
+        
+    html_file = sys.argv[2]
+    sys.exit(0)
