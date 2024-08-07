2a6dfa0d07d37628ba660199492a79eec25a3eca
ksyv <viais.kevin@hotmail.fr>
2024-08-07 12:51:16 +0200

    fix pycode style

--- markdown2html.py
+++ markdown2html.py
@@ -5,8 +5,10 @@ First argument is the name of the Markdown file
 Second argument is the output file name
 Requirements:
 
-If the number of arguments is less than 2: print in STDERR Usage: ./markdown2html.py README.md README.html and exit 1
-If the Markdown file doesn’t exist: print in STDER Missing <filename> and exit 1
+If the number of arguments is less than 2: print in STDERR Usage:
+ ./markdown2html.py README.md README.html and exit 1
+If the Markdown file doesn’t exist:
+print in STDER Missing <filename> and exit 1
 Otherwise, print nothing and exit 0"""
 import sys
 import os
@@ -22,6 +24,6 @@ if __name__ == "__main__":
     if not os.path.exists(markdown_file):
         print("Missing {}".format(markdown_file), file=sys.stderr)
         sys.exit(1)
-        
+
     html_file = sys.argv[2]
     sys.exit(0)
